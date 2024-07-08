b500f81c2131ef6623671b7f153ea0f7a81d8d04
ngovanbang2001 <ngovanbang2001@gmail.com>
2024-07-09 00:36:40 +0700

    rc

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,9 +3,8 @@ import NotFoundError from "../errors/not-found-error.js";
 
 export default {
   async info(req, res) {
+    console.log({ re: req.user});
     if(!req.user) throw new NotFoundError('Not found user')
-    const { email } = req.user
-    if (!email) throw new NotFoundError("Not Found email")
     const data = await userService.info({ email });
     const { password , ...rest} = data
     return res.status(200).json({ user: rest })
